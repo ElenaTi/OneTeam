d49c0c804e9da2b8bb1d7778ccc4a6ab19c80f9f
Elena Timofeeva <elena.posazh@mail.ru>
2024-01-22 20:57:46 +0300

    unsuccess logintest

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -6,16 +6,27 @@ import static com.codeborne.selenide.Selenide.open;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
 public class LoginTests {
 
     @Test
-    void Login(){
+    void Login()
+    {
         //Configuration.browser = "firefox";
         open("https://idev.etm.ru/oneteam/login");
-       /* $(".LoginForm_title__R4WVI ").shouldHave(text("Вход в личный кабинет"));
+        $(".LoginForm_title__R4WVI ").shouldHave(text("Вход в личный кабинет"));
         $("#login").setValue("51951tes");
         $("#password").setValue("heph7146").pressEnter();
-        $(".ant-typography").shouldHave(text("Ежедневник"));*/
+        $(By.xpath("//h1")).shouldHave(text("Ежедневник"));
+    }
+
+    @Test
+    void LoginWithoutData()
+    {
+        Configuration.holdBrowserOpen = true;
+        open("https://idev.etm.ru/oneteam/login");
+        $("[type='submit']").shouldHave((text("войти"))).click();
 
 
     }
