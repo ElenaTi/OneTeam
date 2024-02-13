3d308056fc1a813c8079d3c0cbba78afa60ef1e4
Elena Timofeeva <elena.posazh@mail.ru>
2024-02-13 12:52:12 +0300

    added transition to thr main page from login

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -11,10 +11,10 @@ import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import lib.ui.LoginPageObject;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import lib.ui.LoginPageObject;
+import static com.codeborne.selenide.Condition.visible;
+// import lib.ui.LoginPageObject;
 
 
 public class LoginTests {
@@ -33,15 +33,23 @@ public class LoginTests {
     void GoToLoginPage()
     {
         Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1280";
         open("https://idev.etm.ru/oneteam");
         lib.ui.LoginPageObject.TransitionToSocialNetwork(".Footer_logo_icons__pWS-2","https://idev.etm.ru/");
         lib.ui.LoginPageObject.TransitionToSocialNetwork(".Footer_social_media_icons__qTWJ8 :nth-child(1)", "https://vk.com/etm_company");
         lib.ui.LoginPageObject.TransitionToSocialNetwork(".Footer_social_media_icons__qTWJ8 :nth-child(2) .ant-btn-icon", "https://t.me/etm_company");
-        /*$(".Footer_social_media_icons__qTWJ8 :nth-child(3) .ant-btn-icon").click();
-        switchTo().window(1);*/
+        $(".Footer_social_media_icons__qTWJ8 :nth-child(3) .ant-btn-icon").click();
+        switchTo().window(1);
+        /*String actualUrl = WebDriverRunner.url();
+        assert actualUrl.contains("https://www.youtube.com/");
+        $("#header").shouldHave(text("Компания ЭТМ"));*/
+        Selenide.closeWindow();
+        switchTo().window(0);
         lib.ui.LoginPageObject.TransitionToSocialNetwork(".Footer_social_media_icons__qTWJ8 :nth-child(4) .ant-btn-icon", "https://dzen.ru/etm_company?utm_referer=www.etm.ru&utm_referrer=idev.etm.ru");
         $("[data-menu-id*='login']").click();
         $(".LoginForm_title__R4WVI ").shouldHave(text("Вход в личный кабинет"));
+        $("[class*=LoginPage_btn_back]").click();
+        $(By.xpath("//h1")).shouldHave(text("Управляйте продажами – в одной команде с ЭТМ"));
     }
 
 
